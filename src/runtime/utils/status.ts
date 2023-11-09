51028b57e50826d27f232403d9416e4aaa520d95
Virenbar <Virenbar@users.noreply.github.com>
2023-11-09 18:37:02 +0500

    feat: add color resolver

---
+++ src/runtime/utils/status.ts
@@ -0,0 +1,10 @@
+import type { DiscordStatus } from "../../types";
+
+export function resolveColor(status?: DiscordStatus | null) {
+  switch (status) {
+    case "online": return "#23a55a";
+    case "idle": return "#f0b232";
+    case "dnd": return "#f23f43";
+    default: return "#80848e";
+  }
+}
